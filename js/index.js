158d03d072cd4b0264b07ae7d049b2a7adf9f039
Tejas Trivedi <tejas.trivedi@cerberusnetworks.co.uk>
2015-06-02 17:35:09 +0530

    Changes

--- js/index.js
+++ js/index.js
@@ -86,14 +86,13 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 	
-	
-	
 	document.addEventListener('backbutton', backButtonCallback, false);
 	
-	/*if (device.platform =='iOS' && parseFloat(device.version) >= 7.0) {
-          document.body.style.marginTop = "20px";
-    
-    }*/
+	if (device.platform === 'iOS' && parseFloat(device.version) >= 7.0) {
+        $('.ui-header > *').css('margin-top', function (index, curValue) {
+            return parseInt(curValue, 10) + 20 + 'px';
+        });
+    }
 }
 		
 // alert dialog dismissed
